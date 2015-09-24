7056b663ecd48c2e92919cb3873def178f607304
Sergio Renato <sergio.12.educando@gmail.com>
2015-09-23 23:57:47 -0500

    Vista productos

--- views/web/productos.php
+++ views/web/productos.php
@@ -1,8 +1,106 @@
-<?php
+<div class="col-md-9">
+    <div class="col-md-3">
+        
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Quemadores</a>
+                    <a href="#" class="list-group-item">Proteinas</a>
+                    <a href="#" class="list-group-item">Anabolicos</a>
+                </div>
+        
+    </div>
+    <div class="col-md-9">
+            
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                   <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt="">
+                            <div class="caption">
+                                <h4 class="text-center"><a href="#">Producto1</a>
+                                </h4>
+                                
+                                <h6><strong>PRECIO:</strong> S/. 3.00</h6>
+                                <h6><strong>STOCK:</strong> 3 Unid.</h6>
+                                <p class="text-center"><a href="#">Ver Detalles</a></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    
+                    
 
+                    
+           
+        </div>
+     </div>

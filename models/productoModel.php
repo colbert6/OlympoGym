7a2849fb279aaa37625744f7c070eb941de86f37
Sergio Renato <sergio.12.educando@gmail.com>
2015-09-25 21:42:17 -0500

    Modificacion de Productos

--- models/productoModel.php
+++ models/productoModel.php
@@ -10,10 +10,14 @@
             return $post->fetchall();
         }
         public function getProducto($id){
-
             $post = $this->_db->query("select * from producto where id_producto=$id");
-            return $post->fetchall();
+            return $post->fetch();
         }
+        public function getProductosxCategoria($id){
+             $post = $this->_db->query("select p.nombre as \"producto\",ct.descripcion as \"categoria\" from producto p inner join categoria_producto ct on (p.id_categoria_producto = ct.id_categoria_producto) where p.id_categoria_producto=$id");
+            return $post->fetch();
+        }
+        
     }
     
 ?>

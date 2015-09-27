7b8fd56844d4867605bb13faeba4c930f7d10d9a
Sergio Renato <sergio.12.educando@gmail.com>
2015-09-27 17:10:57 -0500

    Agregue index

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -8,12 +8,7 @@ class indexController extends Controller
     
     public function index()
     {
-        $post = $this->loadModel('post');
-        
-        $this->_view->posts = $post->getPosts();
-        
-        $this->_view->titulo = 'Portada';
-        $this->_view->renderizar('index', 'inicio');
+        $this->_view->renderizar('index');
     }
     
 

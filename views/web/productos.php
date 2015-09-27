d5a0561dd450bdd3c2b34a75dbbdabf91b5d23de
Sergio Renato <sergio.12.educando@gmail.com>
2015-09-27 10:58:12 -0500

    Modifique los productos

--- views/web/productos.php
+++ views/web/productos.php
@@ -19,7 +19,7 @@
                                 </h4>
                                 <p>Para afianzarnos con nuestros clientes</p>
                                 <div class="ratings">
-                                    <p class="text-center"><a href="<?php echo BASE_URL."web/productos/".$this->datos[$i]['descripcion']."/". $this->datos[$i]['id_categoria_producto'];?>" class="btn btn-success">Ver Detalles</a></p>
+                                    <p class="text-center"><a href="<?php echo BASE_URL."web/productos/".$this->datos[$i]['descripcion']."/". $this->datos[$i]['id_categoria_producto'];?>" class="btn btn-success">Ver Productos</a></p>
                                 
                             </div>
                                 
@@ -46,8 +46,7 @@
                  <div class="row">
                         <div class="col-lg-12">
                             <div class="col-lg-12">
-                                <h1 class="page-header">Lista de Productos
-                                </h1>
+                                
                                 <ol class="breadcrumb">
 
                                      <li><a href="<?php echo BASE_URL."web/productos/";?>">Productos</a>
@@ -75,7 +74,10 @@
                 </h4>
                 <p class="text-center"> <strong>Precio:</strong><?php echo " ".$this->datos[$i]['precio']; ?></p>
                 <p class="text-center"><strong>Stock:</strong><?php echo " ".$this->datos[$i]['stock']; ?></p>
-                
+                <div class="ratings">
+                                    <p class="text-center"><a href="<?php echo BASE_URL."web/productos/".$this->datos[$i]['descripcion']."/". $this->datos[$i]['id_categoria_producto'];?>" class="btn btn-warning">Ver Detalles</a></p>
+                                
+                            </div>
                 </div>
             </div>
                 <?php } ?>

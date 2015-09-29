5f221d6203a61a5a223b196e29f67db50312b553
colbert6 <colbersiho_6696@hotmail.com>
2015-09-29 18:58:58 -0500

    Inlcuir Facebook

--- views/web/columna.php
+++ views/web/columna.php
@@ -1,5 +1,6 @@
 <div class="col-md-3">
                 
+                    
                     <div class="row carousel-holder"  >
                                    
                       <div class="panel panel-default">  
@@ -41,9 +42,23 @@
                        </div> 
 
                     </div>
-                
+                <div class="row" >
+                   <div class="panel panel-default"> 
+                   <div class="panel-heading" > 
+                                <span class="glyphicon glyphicon-thumbs-up"></span>
+                                <strong >Visitanos</strong>
+                   </div>     
+                   <div class="fb-page" data-href="https://www.facebook.com/OlympoFitness?ref=ts&amp;fref=ts"
+                        data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
+                        data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore">
+                           <blockquote cite="https://www.facebook.com/OlympoFitness?ref=ts&amp;fref=ts">
+                               <a href="https://www.facebook.com/OlympoFitness?ref=ts&amp;fref=ts">Olympo Fitness</a>
+                           </blockquote></div>
+                   </div>
+                    </div>   
+                </div>
 
-                <div class="row" style="margin-top: 15%;">
+                <div class="row" style="margin-top: 8%">
                      <div >
                         <div class="panel panel-default">  
                             <div class="panel-heading" > 
@@ -56,7 +71,7 @@
                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7928.577112153539!2d-
                                     76.3594834!3d-6.4850947!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91ba0c07765eff75%3A
                                     0xdb6f16d92c725847!2sJiron+San+Martin+422%2C+Tarapoto%2C+Per%C3%BA!5e0!3m2!1ses!
-                                    2s!4v1443068297596" width="100%" height="200px" frameborder="0" style="border:0"
+                                    2s!4v1443068297596" width="100%" height="230px" frameborder="0" style="border:0"
                                     allowfullscreen></iframe>
                             
                           
@@ -65,3 +80,5 @@
                         </div>
                       </div><!-- /.login-box-body -->
                 </div>
+                   
+                </div>   

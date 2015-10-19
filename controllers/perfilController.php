fbd75268715ab1a6cac1b6ad45fa7ebd4bc9e245
colbert6 <colbersiho_6696@hotmail.com>
2015-10-19 13:58:57 -0500

    perfil

--- controllers/perfilController.php
+++ controllers/perfilController.php
@@ -1,7 +1,7 @@
 <?php
 class perfilController extends controller{
 
-    private $_perfil;
+    private $_model;
     private $_id_padre;
     private $_id_hijo;
 
@@ -10,7 +10,7 @@ class perfilController extends controller{
             $this->redireccionar('error/access/5050');
         }*/
         parent::__construct();
-        $this->_perfil = $this->loadModel('perfil');
+        $this->_model = $this->loadModel('perfil');
         
         $this->_modulos->url = 'perfil';
         $modulo= $this->_modulos->selecciona_filtro();
@@ -19,19 +19,19 @@ class perfilController extends controller{
     }
 
     public function index() {
-        $this->_view->datos = $this->_perfil->selecciona();
+        $this->_view->datos = $this->_model->selecciona();
         $this->_view->setCss(array('jquery.dataTables'),true);
         $this->_view->setJs(array('jquery.dataTables.min','run_table'),true);
         $this->_view->titulo = 'Lista de Perfiles de Usuarios';
         $this->_view->renderizar('index',$this->_id_padre,$this->_id_hijo);
     }
     public function nuevo() {
-        if (@$_POST['guardar'] == 1) {
+        if ($_POST['guardar'] == 1) {
             
-            $this->_perfil->descripcion = $_POST['descripcion'];
-            $this->_perfil->nivel = $_POST['nivel'];
+            $this->_model->descripcion = $_POST['descripcion'];
+            $this->_model->nivel = $_POST['nivel'];
             
-            $this->_perfil->inserta();
+            $this->_model->inserta();
             $this->redireccionar('perfil');
         }
         $this->_view->titulo = 'Registrar Prefil de Usuario';
@@ -42,21 +42,22 @@ class perfilController extends controller{
  
     public function editar($id) {
         if (!$this->filtrarInt($id)) {
-            $this->redireccionar('modulo');
+            $this->redireccionar('perfil');
         }
         
         if (@$_POST['guardar'] == 1) {
-            $this->_perfil->descripcion = $_POST['descripcion'];
-            $this->_perfil->nivel = $_POST['nivel'];
+            $this->_model->id_perfil_usuario = $this->filtrarInt($id);
+            $this->_model->descripcion = $_POST['descripcion'];
+            $this->_model->nivel = $_POST['nivel'];
             
-            $this->_perfil->editar();
+            $this->_model->editar();
             $this->redireccionar('perfil');
         }
-        $this->_perfil->id_perfil_usuario = $this->filtrarInt($id);
+        $this->_model->id_perfil_usuario = $this->filtrarInt($id);
         $this->_view->datos = $this->_perfil->selecciona_filtro();
         
-        $this->_view->titulo = 'Actualizar Modulo';
-        $this->_view->action = BASE_URL . 'modulo/editar/'.$id;
+        $this->_view->titulo = 'Actualizar Perfil';
+        $this->_view->action = BASE_URL . 'perfil/editar/'.$id;
         
         //$this->_vista->setJs(array('funciones_form'));
         $this->_view->renderizar('form',$this->_id_padre,$this->_id_hijo);
@@ -64,11 +65,11 @@ class perfilController extends controller{
 
     public function eliminar($id) {
         if (!$this->filtrarInt($id)) {
-            $this->redireccionar('modulo');
+            $this->redireccionar('perfil');
         }
-        $this->_modulos->idmodulo = $this->filtrarInt($id);
-        $this->_modulos->elimina();
-        $this->redireccionar('modulo');
+        $this->_model->id_perfil = $this->filtrarInt($id);
+        $this->_model->elimina();
+        $this->redireccionar('perfil');
     }
     
 }

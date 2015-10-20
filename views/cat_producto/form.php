e388c30213b9b51aa294e56b1d5ad54f3b5d42af
Santos14 <sergio.12.educando@gmail.com>
2015-10-19 20:07:34 -0500

    Aumentando tablas de Mantenimiento

---
+++ views/cat_producto/form.php
@@ -0,0 +1,43 @@
+<!--CATEGORIA EMPLEADO--> 
+<ol class="breadcrumb">
+  <li><a href="<?php echo BASE_URL?>index"  >Inicio</a></li>
+  <li><a href="<?php echo BASE_URL?>cat_producto" >Categoria Empleado</a></li>
+  <li class="active"><?php echo $this->titulo; ?></li>
+</ol>
+
+
+<div class="col-md-8">
+<fieldset>
+    <legend><?php echo $this->titulo; ?></legend>
+    <div class="col-md-1"></div>
+    <div class="col-md-11" style="color:#000">
+    <form class="form-horizontal" role="form" id="form_perfil" method="post" action="<?php echo $this->action; ?>">
+        <input name="guardar" id="guardar" type="hidden" value="1">
+      <?php if(isset ($this->datos[0][0])) {?>  
+      <div class="form-group">
+        <label class="control-label col-sm-3" >IDENTIFICADOR:</label>
+        <div class="col-sm-9">
+            <input name="id_categoria_producto" id="id_categoria_producto" class="form-control"  readonly="readonly"
+                   value="<?php echo $this->datos[0][0];?>">
+        </div>
+      </div>  
+      <?php } ?>  
+      <div class="form-group">
+        <label class="control-label col-sm-3" >DESCRIPCION:</label>
+        <div class="col-sm-9">
+          <input name="descripcion" id="descripcion" class="form-control"  placeholder="Descripcion"
+                value="<?php if(isset ($this->datos[0][1]))echo $this->datos[0][1]?>">
+        </div>
+      </div>
+         
+        <div class="form-group" style="margin-top: 8%"> 
+        <div class="col-sm-offset-3 col-sm-9">
+          <button type="submit" class="btn btn-primary"> Guardar</button>
+          <a style="margin-left: 8%" href="<?php echo BASE_URL?>cat_producto" type="submit" class="btn btn-danger">Cancelar</a>
+        </div>
+      </div>
+
+    </form>
+    </div>
+</fieldset>
+</div>

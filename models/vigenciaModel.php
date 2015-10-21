d4fa1a2603af5bfe4e1952571cc89ea2f58ebd58
Jpoeta95 <yon_keynes95@hotmail.com>
2015-10-21 18:12:16 -0500

    vigencia

---
+++ models/vigenciaModel.php
@@ -0,0 +1,79 @@
+ <?php
+
+class vigenciaModel extends Main{
+
+    public $id_vigencia;
+    public $descripcion;
+    
+    public function selecciona() {
+        $r = $this->get_consulta("pa_m1_vigencia",null);
+        if ($r[1] == '') {
+            $stmt = $r[0];
+        } else {
+            die($r[1]);
+        }
+        $r = null;
+        if (conexion::$_servidor == 'oci') {
+            oci_fetch_all($stmt, $data, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+            return $data;
+        } else {
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            return $stmt->fetchall();
+        }
+        //return $resultado;
+        $r = $this->consulta_simple($sql);
+        return $r;
+        
+    }
+    public function selecciona_filtro() {
+        if (is_null($this->id_vigencia)) {
+            $this->id_vigencia = 0;
+        }
+        if (is_null($this->descripcion)) {
+            $this->descripcion = '';
+        }
+        $sql="SELECT `id_vigencia`, `descripcion` "
+            . "FROM `vigencia` "
+            . "WHERE ( id_vigencia=".$this->id_vigencia." or descripcion='".$this->descripcion."' ) ";
+        
+        $r = $this->consulta_simple($sql);
+        return $r;
+      
+    }
+    
+    public function inserta() {
+        $datos = array($this->descripcion);
+        $r = $this->get_consulta("pa_i_vigencia", $datos);
+        $error = $r[1];
+        $r = null;
+        return $error;
+    }
+
+    public function editar() {
+       
+        $datos = array($this->id_vigencia,$this->descripcion);
+        $r = $this->get_consulta("pa_u_vigencia", $datos);
+        $error = $r[1];
+        $r = null;
+        return $error;
+    }
+
+    
+
+    public function elimina() {
+        $datos = array($this->id_vigencia);
+        $r = $this->get_consulta("pa_d_vigencia", $datos);
+        $error = $r[1];
+        
+        $r = null;
+        return $error;
+    }
+    
+
+}
+
+?>
+
+
+
+
---
+++ views/vigencia/form.php
@@ -0,0 +1,57 @@
+<!--CATEGORIA EMPLEADO--> 
+<ol class="breadcrumb">
+  <li><a href="<?php echo BASE_URL?>index"  >Inicio</a></li>
+  <li><a href="<?php echo BASE_URL?>vigencia" >Vigencia</a></li>
+  <li class="active"><?php echo $this->titulo; ?></li>
+</ol>
+
+
+<div class="col-md-8">
+<fieldset>
+    <legend><?php echo $this->titulo; ?></legend>
+    <div class="col-md-1"></div>
+    <div class="col-md-11" style="color:#000">
+    <form class="form-horizontal" role="form" id="form_perfil" method="post" action="<?php echo $this->action; ?>">
+        <input name="guardar" id="guardar" type="hidden" value="1">
+      <?php if(isset ($this->datos[0][0])) {?>  
+      <div class="form-group">
+        <label class="control-label col-sm-3" >IDENTIFICADOR:</label>
+        <div class="col-sm-9">
+            <input name="id_vigencia" id="id_vigencia" class="form-control"  readonly="readonly"
+                   value="<?php echo $this->datos[0][0];?>">
+        </div>
+      </div>  
+      <?php } ?>  
+      <div class="form-group">
+        <label class="control-label col-sm-3" >DESCRIPCION:</label>
+        <div class="col-sm-9">
+          <input name="descripcion" id="descripcion" class="form-control"  placeholder="Descripcion"
+                value="<?php if(isset ($this->datos[0][1]))echo $this->datos[0][1]?>">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-sm-3" >DURACION:</label>
+        <div class="col-sm-9">
+          <input name="duracion" id="duracion" class="form-control"  placeholder="Duracion"
+                value="<?php if(isset ($this->datos[0][2]))echo $this->datos[0][2]?>">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-sm-3" >UNIDAD DE TIEMPO:</label>
+        <div class="col-sm-9">
+          <input name="unidad_tiempo" id="unidad_tiempo" class="form-control"  placeholder="Unidad de Tiempo"
+                value="<?php if(isset ($this->datos[0][3]))echo $this->datos[0][3]?>">
+        </div>
+      </div>
+         
+        <div class="form-group" style="margin-top: 8%"> 
+        <div class="col-sm-offset-3 col-sm-9">
+          <button type="submit" class="btn btn-primary"> Guardar</button>
+          <a style="margin-left: 8%" href="<?php echo BASE_URL?>vigencia" type="submit" class="btn btn-danger">Cancelar</a>
+        </div>
+      </div>
+
+    </form>
+    </div>
+</fieldset>
+</div>

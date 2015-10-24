714c74c9894d22af1205c80c66ae3510f257c924
Sergio <sergio.12.educando@gmail.com>
2015-10-24 13:52:32 -0500

    Sigo arreglando socio

--- controllers/socioController.php
+++ controllers/socioController.php
@@ -13,6 +13,7 @@ class socioController extends controller{
         parent::__construct();
         $this->_socio = $this->loadModel('socio');
         $this->_tipo_socio = $this->loadModel('tipo_socio');
+        $this->_ubigeo = $this->loadModel('ubigeo');
         
         $this->_modulos->url = 'socio';
         $modulo= $this->_modulos->selecciona_filtro();
@@ -72,26 +73,69 @@ class socioController extends controller{
     }
     public function vermas($id){
         if (!$this->filtrarInt($id)) {
-            $this->redireccionar('cat_empleado');
+            $this->redireccionar('socio');
         }
+        
+        $this->_socio->id_socio = $this->filtrarInt($id);
+        $this->_view->isReadOnly = true;
+        $this->_view->tipo_socio = $this->_tipo_socio->selecciona();
+        $this->_view->ubigeo = $this->_ubigeo->selecciona();
+        $this->_view->datos = $this->_socio->selecciona_filtro();
+
+        $this->_view->titulo = 'Datos del Socio';
+        $this->_view->action = BASE_URL . 'socio/vermas/'.$id;
+
+        $this->_view->renderizar('form',$this->_id_padre,$this->_id_hijo);
+        
+
     }
     public function editar($id) {
         if (!$this->filtrarInt($id)) {
-            $this->redireccionar('cat_empleado');
+            $this->redireccionar('socio');
         }
         
         if (@$_POST['guardar'] == 1) {
-            $this->_socio->id_ambiente = $_POST['id_ambiente'];
-            $this->_socio->descripcion = $_POST['descripcion'];
+            $this->_socio->id_socio = $_POST['id_socio'];
+            $this->_socio->id_tipo_socio = $_POST['id_tipo_socio'];
+            $this->_socio->id_ubigeo = $_POST['id_ubigeo'];
+            $this->_socio->dni = $_POST['dni'];
+            $this->_socio->aliass = $_POST['aliass'];
+            $this->_socio->nombre = $_POST['nombre'];
+            $this->_socio->apellido_paterno = $_POST['apellido_paterno'];
+            $this->_socio->apellido_materno = $_POST['apellido_materno'];
+            $this->_socio->email = $_POST['email'];
+            $this->_socio->telefono = $_POST['telefono'];
+            $this->_socio->celular = $_POST['celular'];
+            $this->_socio->direccion = $_POST['direccion'];
+            $this->_socio->fecha_nacimiento = $_POST['fecha_nacimiento'];
+            $this->_socio->sexo = $_POST['sexo'];
+            $this->_socio->estado_civil = $_POST['estado_civil'];
+            $this->_socio->ocupacion = $_POST['ocupacion'];
+            $this->_socio->grupo_sanguineo = $_POST['grupo_sanguineo'];
+            $this->_socio->hobby = $_POST['hobby'];
+            $this->_socio->nacionalidad = $_POST['nacionalidad'];
+            $this->_socio->seguro_medico = $_POST['seguro_medico'];
+            $this->_socio->observacion = $_POST['observacion'];
+            $this->_socio->antecedente_medico = $_POST['antecedente_medico'];
+            $this->_socio->codigo_postal = $_POST['codigo_postal'];
+            $this->_socio->fax = $_POST['fax'];
+            $this->_socio->numero_hijo = $_POST['numero_hijo'];
+            $this->_socio->sector = $_POST['sector'];
+            $this->_socio->grado_estudio = $_POST['grado_estudio'];
+            $this->_socio->ingresos = $_POST['ingresos'];
+
             $this->_socio->editar();
             
-            $this->redireccionar('ambiente');
+            $this->redireccionar('socio');
         }
-        $this->_socio->id_ambiente = $this->filtrarInt($id);
+        $this->_view->isReadOnly = false;
+        $this->_socio->id_socio = $this->filtrarInt($id);
+        $this->_view->tipo_socio = $this->_tipo_socio->selecciona();
+        $this->_view->ubigeo = $this->_ubigeo->selecciona();
         $this->_view->datos = $this->_socio->selecciona_filtro();
         
-        $this->_view->titulo = 'Actualizar Ambiente';
-        $this->_view->action = BASE_URL . 'ambiente/editar/'.$id;
+        $this->_view->titulo = 'Actualizar Socio';
+        $this->_view->action = BASE_URL . 'socio/editar/'.$id;
         
         //$this->_vista->setJs(array('funciones_form'));
         $this->_view->renderizar('form',$this->_id_padre,$this->_id_hijo);
--- views/socio/form.php
+++ views/socio/form.php
@@ -1,5 +1,12 @@
 
 <!--MODULO--> 
+<?php 
+    if($this->isReadOnly){
+        $bloqueo = "readonly='readonly'";
+    }else{
+        $bloqueo = "";
+    }
+?>
 <ol class="breadcrumb">
   <li><a href="<?php echo BASE_URL?>index"  >Inicio</a></li>
   <li><a href="<?php echo BASE_URL?>socio" >Socio</a></li>
@@ -26,7 +33,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for"nombre">NOMBRE(S):</label>
         <div class="col-sm-9">
-          <input name="nombre" id="nombre" class="form-control"  placeholder="Nombre(s)" autofocus
+          <input <?php echo $bloqueo;?> name="nombre" id="nombre" class="form-control"  placeholder="Nombre(s)" autofocus
                 value="<?php if(isset ($this->datos[0]['NOMBRE']))echo $this->datos[0]['NOMBRE']?>">
         </div>
       </div>
@@ -34,7 +41,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="apellido_paterno" >APELLIDO PATERNO:</label>
         <div class="col-sm-9">
-          <input name="apellido_paterno" id="apellido_paterno" class="form-control"  placeholder="Apellido Paterno" 
+          <input <?php echo $bloqueo;?> name="apellido_paterno" id="apellido_paterno" class="form-control"  placeholder="Apellido Paterno" 
                 value="<?php if(isset ($this->datos[0]['APELLIDO_PATERNO']))echo $this->datos[0]['APELLIDO_PATERNO']?>">
         </div>
       </div>
@@ -42,7 +49,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="apellido_materno" >APELLIDO MATERNO:</label>
         <div class="col-sm-9">
-          <input name="apellido_materno" id="apellido_materno" class="form-control"  placeholder="Apellido Materno" 
+          <input <?php echo $bloqueo;?> name="apellido_materno" id="apellido_materno" class="form-control"  placeholder="Apellido Materno" 
                 value="<?php if(isset ($this->datos[0]['APELLIDO_MATERNO']))echo $this->datos[0]['APELLIDO_MATERNO']?>">
         </div>
       </div>
@@ -50,7 +57,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="aliass" >ALIAS:</label>
         <div class="col-sm-9">
-          <input name="aliass" id="aliass" class="form-control"  placeholder="Alias" 
+          <input <?php echo $bloqueo;?> name="aliass" id="aliass" class="form-control"  placeholder="Alias" 
                 value="<?php if(isset ($this->datos[0]['ALIASS']))echo $this->datos[0]['ALIASS']?>">
         </div>
       </div>
@@ -58,7 +65,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="dni" >DNI:</label>
         <div class="col-sm-9">
-          <input name="dni" id="dni" class="form-control"  placeholder="DNI" 
+          <input <?php echo $bloqueo;?> name="dni" id="dni" class="form-control"  placeholder="DNI" 
                 value="<?php if(isset ($this->datos[0]['DNI']))echo $this->datos[0]['DNI']?>">
         </div>
       </div>
@@ -66,10 +73,10 @@
       <div class="form-group">
         <label class="control-label col-sm-3" >TIPO SOCIO:</label>
         <div class="col-sm-9"> 
-           <select class="form-control glyphicon" name='id_tipo_socio' id='padre'>
+           <select <?php echo $bloqueo;?> class="form-control glyphicon" name='id_tipo_socio' id='padre'>
                <option value='' >Selecciona...</option>
                <?php for($i=0;$i<count($this->tipo_socio);$i++){ ?> 
-                <?php if( $this->datos[0]['TIPO_SOCIO']==$this->tipo_socio[$i]['DESCRIPCION']){?>
+                <?php if( $this->datos[0]['ID_TIPO_SOCIO']==$this->tipo_socio[$i]['ID_TIPO_SOCIO']){?>
                      <option selected value="<?php echo $this->tipo_socio[$i]['ID_TIPO_SOCIO'];?>"><?php echo $this->tipo_socio[$i]['DESCRIPCION']?></option>
                 <?php }else{?>
                      <option value="<?php echo $this->tipo_socio[$i]['ID_TIPO_SOCIO'];?>"><?php echo $this->tipo_socio[$i]['DESCRIPCION']?></option>
@@ -82,10 +89,15 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="id_ubigeo" >UBIGEO:</label>
         <div class="col-sm-9">
-          <select class="form-control glyphicon" name='id_ubigeo' id='id_ubigeo'>
+          <select <?php echo $bloqueo;?> class="form-control glyphicon" name='id_ubigeo' id='id_ubigeo'>
                <option value='' >Selecciona...</option>
-                <option value='1' >OTRO</option>
-                <option value='1' >OTRO1</option>
+               <?php for($i=0;$i<count($this->ubigeo);$i++){ ?> 
+                <?php if( $this->datos[0]['ID_UBIGEO']==$this->ubigeo[$i]['ID_UBIGEO']){?>
+                     <option selected value="<?php echo $this->ubigeo[$i]['ID_UBIGEO'];?>"><?php echo $this->ubigeo[$i]['DEPARTAMENTO'].", ".$this->ubigeo[$i]['PROVINCIA'].", ".$this->ubigeo[$i]['DISTRITO']?></option>
+                <?php }else{?>
+                     <option value="<?php echo $this->ubigeo[$i]['ID_UBIGEO'];?>"><?php echo $this->ubigeo[$i]['DEPARTAMENTO'].", ".$this->ubigeo[$i]['PROVINCIA'].", ".$this->ubigeo[$i]['DISTRITO']?></option>
+                <?php } ?>
+               <?php } ?>
           </select>
         </div>
       </div>
@@ -93,7 +105,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="email" >E-MAIL:</label>
         <div class="col-sm-9">
-          <input name="email" id="email" class="form-control"  type="email" placeholder="E-mail" 
+          <input <?php echo $bloqueo;?> name="email" id="email" class="form-control"  type="email" placeholder="E-mail" 
                 value="<?php if(isset ($this->datos[0]['EMAIL'])) echo $this->datos[0]['EMAIL']?>">
         </div>
       </div>
@@ -101,7 +113,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="telefono" >TELEFONO:</label>
         <div class="col-sm-9">
-          <input name="telefono" id="telefono" class="form-control"  type="text" placeholder="Telefono" 
+          <input <?php echo $bloqueo;?> name="telefono" id="telefono" class="form-control"  type="text" placeholder="Telefono" 
                 value="<?php if(isset ($this->datos[0]['TELEFONO'])) echo $this->datos[0]['TELEFONO']?>">
         </div>
       </div>
@@ -109,15 +121,15 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="celular" >CELULAR:</label>
         <div class="col-sm-9">
-          <input name="celular" id="celular" class="form-control"  type="text" placeholder="Celular" 
+          <input <?php echo $bloqueo;?>  name="celular" id="celular" class="form-control"  type="text" placeholder="Celular" 
                 value="<?php if(isset ($this->datos[0]['CELULAR'])) echo $this->datos[0]['CELULAR']?>">
         </div>
       </div>
 
       <div class="form-group">
-        <label class="control-label col-sm-3" for="direccion" >DIRECION:</label>
+        <label  class="control-label col-sm-3" for="direccion" >DIRECION:</label>
         <div class="col-sm-9">
-          <input name="direccion" id="direccion" class="form-control"  type="text" placeholder="Direccion" 
+          <input <?php echo $bloqueo;?> name="direccion" id="direccion" class="form-control"  type="text" placeholder="Direccion" 
                 value="<?php if(isset ($this->datos[0]['DIRECCION'])) echo $this->datos[0]['DIRECCION']?>">
         </div>
       </div>
@@ -127,7 +139,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="fecha_nacimiento" >FECHA NACIMIENTO:</label>
         <div class="col-sm-9">
-          <input name="fecha_nacimiento" id="fecha_nacimiento" class="form-control"  type="date" placeholder="Fecha Nacimiento" 
+          <input <?php echo $bloqueo;?> name="fecha_nacimiento" id="fecha_nacimiento" class="form-control"  type="date" placeholder="Fecha Nacimiento" 
                 value="<?php if(isset ($this->datos[0]['FECHA_NACIMIENTO'])) echo $this->datos[0]['FECHA_NACIMIENTO']?>">
         </div>
       </div>
@@ -135,7 +147,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="sexo" >SEXO:</label>
         <div class="col-sm-9">
-          <select class="form-control glyphicon" name='sexo' id='sexo'>
+          <select <?php echo $bloqueo;?> class="form-control glyphicon" name='sexo' id='sexo'>
               <?php if(isset ($this->datos[0]['SEXO'])){?>
               <?php     for ($i=0; $i <2 ; $i++) { 
                            if($this->datos[0]['SEXO'] == 1){
@@ -161,7 +173,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="sexo" >ESTADO CIVIL:</label>
         <div class="col-sm-9">
-          <select class="form-control glyphicon" name='estado_civil' id='estado_civil'>
+          <select <?php echo $bloqueo;?> class="form-control glyphicon" name='estado_civil' id='estado_civil'>
                <option value='' >Selecciona...</option>
                <?php if(isset($this->datos[0]['ESTADO_CIVIL'])){?>
                <?php      for ($i=0; $i < 4 ; $i++) { 
@@ -199,7 +211,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="ocupacion" >OCUPACION:</label>
         <div class="col-sm-9">
-          <input name="ocupacion" id="ocupacion" class="form-control"  type="text" placeholder="Ocupacion" 
+          <input <?php echo $bloqueo;?> name="ocupacion" id="ocupacion" class="form-control"  type="text" placeholder="Ocupacion" 
                 value="<?php if(isset ($this->datos[0]['OCUPACION'])) echo $this->datos[0]['OCUPACION']?>">
         </div>
       </div>
@@ -207,7 +219,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="grupo_sanguineo" >GRUPO SANGUINEO:</label>
         <div class="col-sm-9">
-          <input name="grupo_sanguineo" id="grupo_sanguineo" class="form-control"  type="text" placeholder="Grupo Sanguineo" 
+          <input <?php echo $bloqueo;?> name="grupo_sanguineo" id="grupo_sanguineo" class="form-control"  type="text" placeholder="Grupo Sanguineo" 
                 value="<?php if(isset ($this->datos[0]['GRUPO_SANGUINEO'])) echo $this->datos[0]['GRUPO_SANGUINEO']?>">
         </div>
       </div>
@@ -215,7 +227,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="hobby" >HOBBY:</label>
         <div class="col-sm-9">
-          <input name="hobby" id="hobby" class="form-control"  type="text" placeholder="Hobby" 
+          <input <?php echo $bloqueo;?> name="hobby" id="hobby" class="form-control"  type="text" placeholder="Hobby" 
                 value="<?php if(isset ($this->datos[0]['HOBBY'])) echo $this->datos[0]['HOBBY']?>">
         </div>
       </div>
@@ -223,7 +235,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="nacionalidad" >NACIONALIDAD:</label>
         <div class="col-sm-9">
-          <input name="nacionalidad" id="nacionalidad" class="form-control"  type="text" placeholder="Nacionalidad" 
+          <input <?php echo $bloqueo;?> name="nacionalidad" id="nacionalidad" class="form-control"  type="text" placeholder="Nacionalidad" 
                 value="<?php if(isset ($this->datos[0]['NACIONALIDAD'])) echo $this->datos[0]['NACIONALIDAD']?>">
         </div>
       </div>
@@ -231,15 +243,15 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="seguro_medico" >SEGURO MEDICO:</label>
         <div class="col-sm-9">
-          <input name="seguro_medico" id="seguro_medico" class="form-control"  type="text" placeholder="Seguro Medico" 
+          <input <?php echo $bloqueo;?> name="seguro_medico" id="seguro_medico" class="form-control"  type="text" placeholder="Seguro Medico" 
                 value="<?php if(isset ($this->datos[0]['SEGURO_MEDICO'])) echo $this->datos[0]['SEGURO_MEDICO']?>">
         </div>
       </div>
 
       <div class="form-group">
-        <label class="control-label col-sm-3" for="observacion" >OBSERVACION:</label>
+        <label  class="control-label col-sm-3" for="observacion" >OBSERVACION:</label>
         <div class="col-sm-9">
-          <input name="observacion" id="observacion" class="form-control"  type="text" placeholder="Observacion" 
+          <input <?php echo $bloqueo;?> name="observacion" id="observacion" class="form-control"  type="text" placeholder="Observacion" 
                 value="<?php if(isset ($this->datos[0]['OBSERVACION'])) echo $this->datos[0]['OBSERVACION']?>">
         </div>
       </div>
@@ -247,7 +259,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="antecedente_medico" >ANTECEDENTES MEDICOS:</label>
         <div class="col-sm-9">
-          <input name="antecedente_medico" id="antecedente_medico" class="form-control"  type="text" placeholder="Antecedente Medico" 
+          <input <?php echo $bloqueo;?> name="antecedente_medico" id="antecedente_medico" class="form-control"  type="text" placeholder="Antecedente Medico" 
                 value="<?php if(isset ($this->datos[0]['ANTECEDENTE_MEDICO'])) echo $this->datos[0]['ANTECEDENTE_MEDICO']?>">
         </div>
       </div>
@@ -255,7 +267,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="codigo_postal" >CODIGO POSTAL:</label>
         <div class="col-sm-9">
-          <input name="codigo_postal" id="codigo_postal" class="form-control"  type="text" placeholder="Codigo Postal" 
+          <input <?php echo $bloqueo;?> name="codigo_postal" id="codigo_postal" class="form-control"  type="text" placeholder="Codigo Postal" 
                 value="<?php if(isset ($this->datos[0]['CODIGO_POSTAL'])) echo $this->datos[0]['CODIGO_POSTAL']?>">
         </div>
       </div>
@@ -263,7 +275,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="fax" >FAX:</label>
         <div class="col-sm-9">
-          <input name="fax" id="fax" class="form-control"  type="text" placeholder="Fax" 
+          <input <?php echo $bloqueo;?> name="fax" id="fax" class="form-control"  type="text" placeholder="Fax" 
                 value="<?php if(isset ($this->datos[0]['FAX'])) echo $this->datos[0]['FAX']?>">
         </div>
       </div>
@@ -271,7 +283,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="numero_hijo" >NUMERO DE HIJOS:</label>
         <div class="col-sm-9">
-          <input name="numero_hijo" id="numero_hijo" class="form-control"  type="number" placeholder="Numero de Hijos" 
+          <input <?php echo $bloqueo;?> name="numero_hijo" id="numero_hijo" class="form-control"  type="number" placeholder="Numero de Hijos" 
                 value="<?php if(isset ($this->datos[0]['NUMERO_HIJO'])) echo $this->datos[0]['NUMERO_HIJO']?>">
         </div>
       </div>
@@ -280,7 +292,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="sector" >SECTOR:</label>
         <div class="col-sm-9">
-          <input name="sector" id="sector" class="form-control"  type="text" placeholder="Sector" 
+          <input <?php echo $bloqueo;?> name="sector" id="sector" class="form-control"  type="text" placeholder="Sector" 
                 value="<?php if(isset ($this->datos[0]['SECTOR'])) echo $this->datos[0]['SECTOR']?>">
         </div>
       </div>
@@ -288,7 +300,7 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="grado_estudio" >GRADO DE ESTUDIO:</label>
         <div class="col-sm-9">
-          <input name="grado_estudio" id="grado_estudio" class="form-control"  type="text" placeholder="Grado de Estudio" 
+          <input <?php echo $bloqueo;?> name="grado_estudio" id="grado_estudio" class="form-control"  type="text" placeholder="Grado de Estudio" 
                 value="<?php if(isset ($this->datos[0]['GRADO_ESTUDIO'])) echo $this->datos[0]['GRADO_ESTUDIO']?>">
         </div>
       </div>
@@ -296,8 +308,8 @@
       <div class="form-group">
         <label class="control-label col-sm-3" for="ingreso" >INGRESO:</label>
         <div class="col-sm-9">
-          <input name="ingresos" id="ingreso" class="form-control"  type="text" placeholder="Ingreso" 
-                value="<?php if(isset ($this->datos[0]['INGRESO'])) echo $this->datos[0]['INGRESO']?>">
+          <input <?php echo $bloqueo;?> name="ingresos" id="ingreso" class="form-control"  type="text" placeholder="Ingreso" 
+                value="<?php if(isset ($this->datos[0]['INGRESOS'])) echo $this->datos[0]['INGRESOS']?>">
         </div>
       </div>
 
@@ -305,9 +317,15 @@
 
         <div class="form-group" style="margin-top: 8%"> 
         <div class="col-sm-offset-3 col-sm-9">
+          <?php if(!$this->isReadOnly){?>
           <button type="submit" class="btn btn-primary"> Guardar</button>
           <a style="margin-left: 8%" href="<?php echo BASE_URL?>socio" type="submit" class="btn btn-danger">Cancelar</a>
+          <?php }else{?>
+            <a style="margin-left: 8%" href="<?php echo BASE_URL?>socio" type="submit" class="btn btn-danger">Volver Atras</a>
+          <?php }?>
         </div>
+
+
       </div>
 
     </form>
